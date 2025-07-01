caa5b10206ff26721f1d71790cc01402172cd669
Franco Bossi <fbossi2003@gmail.com>
2025-07-01 11:28:47 -0300

    paso el slide1 a codigo

--- landing-site/src/LandingPage.js
+++ landing-site/src/LandingPage.js
@@ -12,6 +12,8 @@ import slide4 from './slide4-landing.svg'; // tu SVG amarillo con la foto
 import procesoImg from './topics.svg'; // O la ruta que uses
 import logoImg from './logo.svg';
 
+import slide1code from './slide1code.png'; 
+
 
 
 
@@ -152,6 +154,23 @@ export default function LandingPage() {
         {sent ? '¡Gracias por registrarte!' : 'Enviar'}
       </button>
     </form>
+</section>
+
+
+<section className={styles.slidecodeSection}>
+    <div className={styles.slidecode}>
+      <div>
+        <h4 className={styles.introSlide}>Para Marcas</h4>
+        <h2 className={styles.formTitle}>Aliate con creadores, sin estrés</h2>
+        <p>Creamos la infraestructura para que tu marca colabore con creadores sin perder tiempo.
+          Encontrá, coordiná y medí resultados desde una sola plataforma.</p>
+      </div>
+      <img
+        src={slide1code}
+        alt="Slide para marcas"
+        className={styles.slideImage}
+      />
+    </div>
 </section>
 
 
